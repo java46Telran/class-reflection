a97e089a091ffdc8234e0abd11111a96ed4aa4a0
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-15 21:24:20 +0200

    CW #49 Annotations

---
+++ src/telran/annotation/Id.java
@@ -0,0 +1,14 @@
+package telran.annotation;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({FIELD})
+public @interface Id {
+
+}
---
+++ src/telran/annotation/examples/X.java
@@ -0,0 +1,8 @@
+package telran.annotation.examples;
+
+import telran.annotation.*;
+
+public class X {
+	@Pattern(value = ".*")
+int x;
+}
